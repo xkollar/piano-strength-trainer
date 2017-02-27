22ba48c16b943b52a18e02d222015ddda974d65b
Matej Kollar <208115@mail.muni.cz>
2017-02-27 22:12:22 +0100

    Reorganize file...

--- pokus.hs
+++ pokus.hs
@@ -25,6 +25,30 @@ canvasH = 600
 canvasDim :: Size
 canvasDim = (canvasW, canvasH)
 
+midiMagic :: Line -> DeviceID -> IO ()
+midiMagic l _n = runHuu $ withTestEvents pe'
+-- midiMagic l n = withDeviceStream n $ runHuu . withEvents pe'
+  where
+    pe' = withPMMsg pe
+    pe :: PMMsg -> Huu ()
+    pe ev@PMMsg{..} = when (status == midiDown) $ do
+        lift $ print ev
+        modify $ Map.insert k v
+        s <- gets Map.toList
+        -- As it turns out, we have to pass at least two points...
+        when (length s >= 2) $
+            void . lift $ l # coord (map whoop s)
+      where
+        whoop = fixInst *** fixStren
+
+        fixInst x = fromIntegral x * canvasW `div` 127
+
+        fixStren x = (127-fromIntegral x) * canvasH `div` 127
+
+        k = fromIntegral data1
+
+        v = fromIntegral data2
+
 mainGr :: DeviceID -> IO ()
 mainGr n = do
     mainW <- initHTk [text "MIDI Plot"]
@@ -54,27 +78,3 @@ main' _ = putStrLn "usage: $PROG [DEVICE_ID]"
 
 main :: IO ()
 main = Midi.initialize >> getArgs >>= main'
-
-midiMagic :: Line -> DeviceID -> IO ()
-midiMagic l _n = runHuu $ withTestEvents pe'
--- midiMagic l n = withDeviceStream n $ runHuu . withEvents pe'
-  where
-    pe' = withPMMsg pe
-    pe :: PMMsg -> Huu ()
-    pe ev@PMMsg{..} = when (status == midiDown) $ do
-        lift $ print ev
-        modify $ Map.insert k v
-        s <- gets Map.toList
-        -- As it turns out, we have to pass at least two points...
-        when (length s >= 2) $
-            void . lift $ l # coord (map whoop s)
-      where
-        whoop = fixInst *** fixStren
-
-        fixInst x = fromIntegral x * canvasW `div` 127
-
-        fixStren x = (127-fromIntegral x) * canvasH `div` 127
-
-        k = fromIntegral data1
-
-        v = fromIntegral data2
